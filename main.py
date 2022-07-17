582362084766c6007a81c2a0f36b9a24d14cd708
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-07-17 15:45:47 +0700

    add country method for get parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from email import message
+import re
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def index():
   return {"message" : "Hello World"}
+
+@app.get("/countries/{country_name}")
+async def country(country_name):
+  return {"country_name": country_name}
