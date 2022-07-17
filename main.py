398d11fe8c89715e837b030038e40469f0956855
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2022-07-17 16:15:01 +0700

    request body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -21,3 +22,23 @@ async def country(country_name: str = "japan", country_no: Optional[int] = None)
     "country_name": country_name,
     "country_no": country_no
   }
+
+class Item(BaseModel):
+  name: str
+  description: Optional[str] = None
+  price: int
+  tax: Optional[float] = None
+
+"""
+sample item
+{
+    "name": "Foo",
+    "description": "An optional description",
+    "price": 45.2,
+    "tax": 3.5
+}
+"""
+
+@app.post("/item")
+async def create_item(item: Item):
+  return {"message": f"{item.name}は、税込価格{int(item.price*(1 + item.tax))}円です。"}
